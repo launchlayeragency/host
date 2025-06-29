92a10cc83e57771487c13b9f2108a6780e9c6b45
Soraiya11-7 <soraiyaparvin@gmail.com>
2025-06-29 17:27:53 +0600

    News Details and News page route added on router page and hero section added for both of these

---
+++ src/Pages/NewsDetails/NewsDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NewsDetailsHero from '../../components/NewsDetails/NewsDetailsHero'
+
+export default function NewsDetails() {
+  return (
+    <div>
+        <NewsDetailsHero/>
+    </div>
+  )
+}
