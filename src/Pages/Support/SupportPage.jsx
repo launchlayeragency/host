64ec5341f5f66231e70a51ae7e707e4f793ee1fa
Soraiya11-7 <soraiyaparvin@gmail.com>
2025-06-29 16:16:02 +0600

    Support page done under Pages

--- src/Pages/Support/SupportPage.jsx
+++ src/Pages/Support/SupportPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import PricingSection from '../../components/AboutUs/PricingSection'
 import SupportHero from '../../components/Support/SupportHero'
+import SupportFeatures from '../../components/Support/SupportFeatures'
+import SupportCards from '../../components/Support/SupportCards'
 
 export default function SupportPage() {
   return (
     <div>
         <SupportHero/>
+        <SupportCards/>
+        <SupportFeatures/>
           <PricingSection scrollToId="vpsHosting"/>
     </div>
   )
